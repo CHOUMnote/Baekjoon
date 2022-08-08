05acf7dae8fac6388afefd293424b0ca6e9e4f1a
KIMJiho <52690007+CHOUMnote@users.noreply.github.com>
2022-08-08 15:46:37 +0900

    Update 단지번호붙이기.py
    풀이 추가

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -33,3 +33,39 @@ for i in range(A):
 print(len(result))
 for i in sorted(result):
     print(i)
+    
+'''
+#BFS풀이
+A = int(input())
+a = [input() for i in range(A)]
+
+result = []
+q = [] #bfs 시점 시퀸스
+
+for i,v in enumerate(a):
+    a[i] = list(v)
+
+def bfs(x,y):
+    q.append((x,y))
+    moveX = [0,0,-1,1]
+    moveY = [1,-1,0,0]
+    count = 0
+    while q:
+        x,y = q.pop(0)
+        if(x<0 or y<0 or x>=A or y>=A) : continue
+        if(a[x][y] == '1'):
+            count+=1
+            a[x][y] = '0'
+            for i in range(4):
+                q.append((x+moveX[i],y+moveY[i]))
+    return count
+
+for i in range(A):
+    for j in range(A):
+        if(a[i][j] != '0'):
+            result.append(bfs(i,j))
+            
+print(len(result))
+for i in sorted(result):
+    print(i)
+'''
