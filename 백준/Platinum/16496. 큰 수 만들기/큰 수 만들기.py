8e96f387b176684d4dbbbb74cd4d015b7d8941df
KIMJiho <52690007+CHOUMnote@users.noreply.github.com>
2022-11-29 13:45:49 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -1,2 +1,13 @@
-n,arr = input(),input().split()
-print(int("".join(list(zip(*sorted([(int((arr[i]*10)[:10]),arr[i]) for i in range(int(n))],key=lambda x: -x[0])))[1])))
+n = input()
+arr = list(map(int,input().split()))
+arrC = []
+result = ""
+cnt = 0
+for i in arr:
+    x = str(i)
+    while len(x) <= 10 : x+=x
+    arrC.append((int(x[:10]),cnt))
+    cnt+=1
+for i in sorted(arrC,key=lambda x: -x[0]):
+    result+=str(arr[i[1]])
+print(result if int(result) != 0 else 0)
