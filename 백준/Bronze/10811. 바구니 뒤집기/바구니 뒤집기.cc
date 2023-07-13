bfafc93c95589003465b42ae12f6f27441a769b7
KIMJiho <52690007+CHOUMnote@users.noreply.github.com>
2023-07-13 15:27:48 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    vector<int> arr(101);
+    int n, m, a, b;
+    cin >> n >> m;
+    for (int i = 1; i <= 100; i++)
+        arr[i] = i;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        reverse(arr.begin()+a,arr.begin()+b+1);
+    }
+
+    for (int i = 1; i <= n; i++)
+        cout << arr[i] << ' ';
+
+}
